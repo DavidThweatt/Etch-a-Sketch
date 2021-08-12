29e0c3cf6b129ae2823e733f2dda4b791883fac4
DavidThweatt <ddthweatt9@gmail.com>
2021-08-12 15:18:26 -0700

    fixed grid and started javascript logic

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const default_color = black;
-const default_mode = 'color';
-const default_size = 16;
-
-let currentColor = default_color;
-let currentMode = default_mode;
-let currentSize = default_size;
-
-
 const container = document.getElementById("container");
 const colorWheel = document.getElementById("primary_color");
 const colorBTN = document.getElementById("color");
@@ -15,68 +6,83 @@ const eraserBTN = document.getElementById("eraser");
 const clearBTN = document.getElementById("clear");
 const slider = document.getElementById("slider");
 const colorDiv = document.getElementById("color_val");
+const gridElement = document.createElement('div');
+const default_color = '#000000';
+const default_mode = 'color';
+const default_size = '16px';
 
+let currentColor = default_color;
+let currentMode = default_mode;
+let currentSize = default_size;
+
+function setCurrentColor(newColor) {
+  currentColor = newColor;
+};
+
+function setCurrentMode(newMode) {
+  activateButton(newMode);
+  currentMode = newMode;
+};
+
+function setCurrentSize(newSize) {
+  currentSize = newSize;
+};
 
 function setupGrid(size) {
   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  
 
   for (let i = 0; i < size * size; i++) {
-    const gridElement = document.createElement('div');
-    gridElement.addEventListener('mouseover', changeColor);
+    const gridElement = document.createElement('div')
+    gridElement.className = 'grid-item';
     container.appendChild(gridElement);
   };
 };
 
-colorWheel.onchange = function() {
-  colorDiv.style.color = colorWheel.value;
-  activate();
-}
+setupGrid(28);
 
-colorBTN.addEventListener('click', (e) => {
-  colorMode();
+colorWheel.addEventListener('change', (e) => {
+  setCurrentColor(e.target.value); 
+});
+colorBTN.addEventListener('click', () => {
+  setCurrentMode('color');
+  colorBTN.classList.add('clicked');
+});
+rainbowBTN.addEventListener('click', () => {
+  setCurrentMode('rainbow');
+});
+eraserBTN.addEventListener('click', () => {
+  setCurrentMode('eraser');
+});
+clearBTN.addEventListener('click', () => {
+  clearAll('clear');
+});
+slider.addEventListener('input', (e) => {
+  setupGrid(e.target.value);
+});
+gridElement.addEventListener('mouseover', () => {
+  e.target.classlist.add('active')
 });
 
-rainbowBTN.addEventListener('click', (e) => {
-  rainbowMode();
-});
 
-eraserBTN.addEventListener('click', (e) => {
-  eraserMode();
-});
+function changeColor() {
+  const randoNum = Math.floor(Math.random() * 255);
 
-clearBTN.addEventListener('click', (e) => {
-  clearAll();
-});
+  if (currentMode === rainbowMode) {
+    gridElement.style.color = "rgb(randoNum, randoNum, randoNum)"; 
+  } else if (currentMode === color) {
 
-slider.addEventListener('click', (e) => {
-  makeRows;
-});
+  } else if (currentMode === eraser) {
 
-container.addEventListener('mouseover', (e) => {
-  activate();
-});
+  };
 
-// activate() {
-   // mouse over color pad actives coloring
-// }
+};
 
-// function clearAll {
-//   container background-color: white;
-// }
+function clearAll() {
+  const allGrid = document.querySelectorAll(gridElement);
 
-// function eraserMode() {
-//   mouse over turns cell white.
-// };
-
-// fucntion colorMode() {
-//   turns cells chosen color with mouse over.
-// };
-     
-// fucntion rainbowMode() {
-//    turns cells random rbg value with .math on mouse over
-// }
-
-window.onload = () => {
-  setupGrid(default_size);
+  if (currentMode === clear) {
+    allGrid.classlist.add('white');
+  }
 }
